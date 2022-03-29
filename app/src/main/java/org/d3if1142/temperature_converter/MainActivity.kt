0291e3cdbaa296182297b07b80d9089bbd0581de
david_marsen_purba <dmonap@student.telkomuniversity.ac.id>
2022-03-29 14:14:22 +0700

    Menambah view binding

--- app/src/main/java/org/d3if1142/temperature_converter/MainActivity.kt
+++ app/src/main/java/org/d3if1142/temperature_converter/MainActivity.kt
@@ -2,10 +2,25 @@ package org.d3if1142.temperature_converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import org.d3if1142.temperature_converter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.convert.setOnClickListener { convertSuhu() }
+
+    }
+
+    private fun  convertSuhu(){
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
