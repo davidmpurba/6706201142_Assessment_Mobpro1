0e61998517d64b5043dc593a494b815cc80f896b
david_marsen_purba <dmonap@student.telkomuniversity.ac.id>
2022-03-31 11:10:55 +0700

    Membuat perhitungan converter suhu

--- app/src/main/java/org/d3if1142/temperature_converter/MainActivity.kt
+++ app/src/main/java/org/d3if1142/temperature_converter/MainActivity.kt
@@ -1,14 +1,16 @@
 package org.d3if1142.temperature_converter
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
+import android.text.TextUtils
+import android.widget.Toast
 import org.d3if1142.temperature_converter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private  lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +18,54 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.convert.setOnClickListener { convertSuhu() }
+        binding.convert.setOnClickListener { hasilSuhu() }
 
     }
 
-    private fun  convertSuhu(){
-        Log.d("MainActivity", "Tombol diklik!")
+
+    private  fun hasilSuhu() {
+
+        val nilai = binding.nilaiSuhuInp.text.toString()
+        if (TextUtils.isEmpty(nilai)) {
+            Toast.makeText(this, R.string.nilai_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        if (selectedId == -1) {
+            Toast.makeText(this, R.string.jenis_invalid, Toast.LENGTH_LONG).show()
+            return
+        }else{
+            if (selectedId == R.id.celcius){
+                val cToKelvin = nilai.toFloat()+273.15
+                val cToFahren = 9.0/5.0 * nilai.toFloat() + 32
+                binding.hasil1.text = getString(R.string.hasil_convert2,cToKelvin)
+                binding.hasil2.text = getString(R.string.hasil_convert3,cToFahren)
+            }
+            if (selectedId == R.id.fahrenheit){
+                val fToCelsi = 5.0/9.0 * (nilai.toFloat() - 32)
+                val fToKelvin = 5.0/9.0 * (nilai.toFloat() - 32) + 273.15
+                binding.hasil1.text = getString(R.string.hasil_convert1,fToCelsi)
+                binding.hasil2.text = getString(R.string.hasil_convert2,fToKelvin)
+            }
+            if (selectedId == R.id.kelvin){
+                val kToCelsi= nilai.toFloat() - 273.15
+                val kToFahren = 9.0/5.0 * (nilai.toFloat() - 273.15) + 32
+                binding.hasil1.text = getString(R.string.hasil_convert1,kToCelsi)
+                binding.hasil2.text = getString(R.string.hasil_convert2,kToFahren)
+            }
+        }
+
     }
+
+
+
+
+
+
 }
+
+
+
+
