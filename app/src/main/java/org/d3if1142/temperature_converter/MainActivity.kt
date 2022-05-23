dc5d783c1a9e8dda5f5c135595bca82357e21f89
david_marsen_purba <dmonap@student.telkomuniversity.ac.id>
2022-05-23 13:43:14 +0700

    Refactor fitur hitung ke fragment

--- app/src/main/java/org/d3if1142/temperature_converter/MainActivity.kt
+++ app/src/main/java/org/d3if1142/temperature_converter/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_hitung)
+        setContentView(R.layout.activity_main)
     }
 }
 
--- app/src/main/java/org/d3if1142/temperature_converter/model/HasilConvert.kt
+++ app/src/main/java/org/d3if1142/temperature_converter/model/HasilConvert.kt
@@ -1,9 +1,5 @@
 package org.d3if1142.temperature_converter.model
 
-import org.d3if1142.temperature_converter.KategoriTermometer
-import org.d3if1142.temperature_converter.R
-import org.d3if1142.temperature_converter.databinding.ActivityMainBinding
-
 data class HasilConvert(
     val hasilCelcius: Float,
     val hasilFahrenheit: Float,
